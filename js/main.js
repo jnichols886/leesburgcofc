6680d8dfb7041fe9f15cb44cd349b2970f712f2a
James Nichols <jnichols@generousgod.com>
2023-01-14 20:49:52 -0500

    adding routing and mission page with some styling at bottom of page.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,47 @@
 $(function(){
-  $("#leadership_lnk").click(function(){
-   $("#main_title_div").hide();
+
+    $(window).on('hashchange', function(e){
+        showByHash( );
+        //  alert(hash+"_div");
+    });
+
+
+
+    showByHash();
+
+    function showByHash () {
+        let hash = location.hash.replace( /^#/, '' );
+        if (hash) {
+            document.getElementById(hash+"_div").scrollIntoView();
+            showDiv(hash+"_div");
+        } else {
+            showDiv("main_div");
+        }
+    }
+
+
+
+    function showDiv(theDiv) {
+        $("#main_div").hide();
+        $("#expect_div").hide();
+        $("#leadership_div").hide();
+        $("#map_div").hide();
+        $("#contact_div").hide();
+        $("#submit_div").hide();
+        $("#thanks_div").hide();
+        $("#believe_div").hide();
+        $("#homeless_div").hide();
+        $("#missions_div").hide();
+        $("#" + theDiv).show();
+    }
+
+    /*
+    * <div class="container-fluid" id="expect_div"><h3>expect</h3></div>
+<div class="container-fluid" id="map_div"><h3>map</h3>map</div>
+<div class="container-fluid" id="contact_div"><h3>contact</h3>contact</div>
+<div class="container-fluid" id="submit_div"><h3>submit</h3>submit</div>
+<div class="container-fluid" id="thanks_div"><h3>thanks</h3>Thanks</div>
+    *  */
 });
-  
-  });
 
   
